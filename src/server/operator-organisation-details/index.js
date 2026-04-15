931e8c44e4a369a5e9dd95e2b8166b52445002a6
ayshashaikh <aysha.s.shaikh@gmail.com>
2026-04-15 13:56:30 +0100

    perttier check

--- src/server/operator-organisation-details/index.js
+++ src/server/operator-organisation-details/index.js
@@ -18,7 +18,7 @@ export const operatorOrganisationDetails = {
         {
           method: 'GET',
           path: '/{language}/operator-organisation-details/{companiesHouseNo}',
-          ...operatorOrganisationDetailsController 
+          ...operatorOrganisationDetailsController
         }
       ])
     }
