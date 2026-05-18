7d2f6bfcd0045e95cdab1b67c2130e0ff3b25b15
mja <michael.o.asante@gmail.com>
2026-05-18 15:47:48 +0100

    featrure/RA-119-Mongo-Persistence|Revert operator list from dynamic to static list

--- src/server/operator/controller.js
+++ src/server/operator/controller.js
@@ -1,24 +1,11 @@
-import { apiClient } from '../common/api-client.js'
 import { getLocaleAndTranslator } from '../common/helpers/get-locale-translator.js'
 
 export const operatorController = {
-  async handler(request, h) {
+  handler(request, h) {
     const { t } = getLocaleAndTranslator(request)
-
-    const organisations = await apiClient.get('/organisation')
-    const orgsWithApplications = await Promise.all(
-      organisations.map(async (org) => {
-        const applications = await apiClient.get(
-          `/api/v1/accreditation-applications/${org.orgId ?? org.id}`
-        )
-        return { org, applications }
-      })
-    )
-
     return h.view('operator/index', {
       pageTitle: t('pages.operator.title'),
-      heading: t('pages.operator.heading'),
-      orgsWithApplications
+      heading: t('pages.operator.heading')
     })
   }
 }
